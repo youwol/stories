3aa7acb79149169cbeb46640053b9a8119f63831
greinisch <greinisch@youwol.com>
2021-10-13 15:12:43 +0200

    :wrench: jest config => do not watch 'main.ts' to remove error

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   reporters: [ "default", "jest-junit"],
-  modulePathIgnorePatterns : ["/dist"]
+  modulePathIgnorePatterns : [
+    "/dist", 
+    // if included jest complains about top-level await
+    "/app/main.ts"
+  ]
 };
