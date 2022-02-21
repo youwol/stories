c4c011a9f2a2cfcd6def162b126505b3e9191cb1
greinisch <greinisch@youwol.com>
2022-02-21 17:35:15 +0100

    ♻️ flux-app plugin => improve 'isComponent' logic

--- src/app/grapes-editor/plugins/flux-app/flux-app.component.ts
+++ src/app/grapes-editor/plugins/flux-app/flux-app.component.ts
@@ -2,17 +2,25 @@ import * as grapesjs from 'grapesjs'
 
 import { render } from './script'
 
+const componentType = 'flux-app'
+
 export function fluxAppComponent(editor: grapesjs.Editor) {
-    editor.DomComponents.addType('flux-app', {
+    editor.DomComponents.addType(componentType, {
         extendFn: ['initialize'],
         isComponent: (el: HTMLElement) => {
-            return el.tagName == 'FLUX-APP'
+            return (
+                el.getAttribute &&
+                el.getAttribute('componentType') == componentType
+            )
         },
         model: {
             defaults: {
                 script: render,
                 tagName: 'flux-app',
                 droppable: false,
+                attributes: {
+                    componentType,
+                },
                 traits: [
                     {
                         name: 'app',
