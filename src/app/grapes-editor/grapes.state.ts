f003907d6a8515b4a0fdab0daf45e00c69ada04c
greinisch <greinisch@youwol.com>
2022-02-21 11:10:09 +0100

    ✨ grapes => add latex plugin

--- src/app/grapes-editor/grapes.state.ts
+++ src/app/grapes-editor/grapes.state.ts
@@ -39,6 +39,7 @@ import { filter, map, mergeMap } from 'rxjs/operators'
 import { Page } from '../models'
 import { getMiscBlocks } from './plugins/misc.blocks'
 import { markdownComponent } from './plugins/mardown.component'
+import { latexComponent } from './plugins/latex.component'
 
 export function grapesConfig({
     canvas,
@@ -83,7 +84,7 @@ export function grapesConfig({
             blocks: [...getBlocks(), ...getMiscBlocks()],
         },
         layerManager: { appendTo: layers },
-        plugins: [markdownComponent],
+        plugins: [markdownComponent, latexComponent],
     }
 }
 
