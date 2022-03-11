e25c27afe17c04fb42c8f35db4745024ed974c38
greinisch <greinisch@youwol.com>
2022-03-11 15:50:29 +0100

    🔥 remove tests (not relevant anymore)

--- tests-backup/mock-data/test-story-markdown-code.ts
+++
@@ -1,8 +0,0 @@
-export const contentMarkdownCode = `
-#  Some example of code
-
-## Javascript
-\`\`\`javascript
-let i = 0
-\`\`\`
-`
--- tests-backup/mock-data/test-story-root.ts
+++
@@ -1,3 +0,0 @@
-export const contentRoot = `
-# This is the test data for unit tests for 'Stories'
-`
