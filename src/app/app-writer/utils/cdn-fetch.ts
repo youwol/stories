ac5ce0346e7e2c8ac3d3591670ac0138ea954e86
greinisch <greinisch@youwol.com>
2022-05-27 09:55:15 +0200

    🔥 remove commented code

--- src/app/app-writer/utils/cdn-fetch.ts
+++ src/app/app-writer/utils/cdn-fetch.ts
@@ -11,16 +11,7 @@ export function fetchCodeMirror$(withInstall: CodeRequirements) {
     return from(
         install({
             modules: [...(withInstall?.modules || []), 'codemirror'],
-            scripts: [
-                ...(withInstall?.scripts || []),
-                /*'codemirror#5.52.0~mode/javascript.min.js',
-                'codemirror#5.52.0~mode/markdown.min.js',
-                'codemirror#5.52.0~mode/css.min.js',
-                'codemirror#5.52.0~mode/xml.min.js',
-                'codemirror#5.52.0~mode/htmlmixed.min.js',
-                'codemirror#5.52.0~mode/gfm.min.js',
-                */
-            ],
+            scripts: [...(withInstall?.scripts || [])],
             css: [
                 'codemirror#5.52.0~codemirror.min.css',
                 'codemirror#5.52.0~theme/blackboard.min.css',
