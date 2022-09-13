ed34afb7c07e858d8b9d6e1ed3f804e56a40d81d
youwol-guillaume <greinisch@youwol.com>
2022-09-13 16:48:18 +0200

    :wrench: update template.py + config files

--- README.md
+++ README.md
@@ -1,21 +1,32 @@
-# Stories
+# @youwol/stories
 
-The [**Stories** application](https://platform.youwol.com/applications/@youwol/stories/latest) is a 
-hierarchical document editor. 
+YouWol Stories application
 
-User guide can be found [here](https://l.youwol.com/doc/@youwol/stories).
+This library is part of the hybrid cloud/local ecosystem
+[YouWol](https://platform.youwol.com/applications/@youwol/platform/latest).
 
-Developers' documentation, coverage and bundle's analysis can be found
-[here](https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/stories).
+## Links
 
-## Installation, Build & Test
+[Running app.](https://platform.youwol.com/applications/@youwol/stories/latest)
+
+[Online user-guide](https://l.youwol.com/doc/@youwol/stories)
+
+[Developers documentation](https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/stories)
+
+[Package on npm](https://www.npmjs.com/package/@youwol/stories)
+
+[Source on GitHub](https://github.com/youwol/stories)
+
+# Installation, Build, Test
 
 To install the required dependencies:
 
 ```shell
 yarn
 ```
+
 ---
+
 To build for development:
 
 ```shell
@@ -27,29 +38,37 @@ To build for production:
 ```shell
 yarn build:prod
 ```
+
 ---
-Tests require [py-youwol](https://l.youwol.com/doc/py-youwol)
-to run on port 2001 using the configuration defined [here](https://github.com/youwol/integration-tests-conf).
+
+To run tests:
 
 ```shell
 yarn test
 ```
+
+Coverage can be evaluated using:
+
+```shell
+yarn test-coverage
+```
+
 ---
+
 To start the 'dev-server':
+
 - add `CdnOverride(packageName="@youwol/stories", port=3001)` in your
   [YouWol configuration file](https://l.youwol.com/doc/py-youwol/configuration)
   (in the `dispatches` list).
-- run [py-youwol](https://l.youwol.com/doc/py-youwol)
-- then execute
-  ```shell
-  yarn start
-  ```
+- start [py-youwol](https://l.youwol.com/doc/py-youwol)
+- then execute `yarn start`
 
-Then, browse to the url `http://localhost:2000/applications/@youwol/stories/latest`
-> the port `2000` is the default port for py-youwol, it can be redefined in your py-youwol's configuration file.
+The application can be accessed [here](http://localhost:2000/applications/@youwol/stories/latest) (providing py-youwol
+running using the default port `2000`).
+
 ---
 
-To generate code documentation:
+To generate code's documentation:
 
 ```shell
 yarn doc
---
+++ src/auto-generated.ts
@@ -0,0 +1,90 @@
+
+const runTimeDependencies = {
+    "load": {
+        "@youwol/os-core": "^0.1.0",
+        "@youwol/fv-tree": "^0.2.0",
+        "@youwol/os-top-banner": "^0.1.0",
+        "@youwol/cdn-client": "^1.0.0",
+        "@youwol/http-clients": "^1.0.0",
+        "@youwol/flux-view": "^1.0.0",
+        "@youwol/fv-context-menu": "^0.1.0",
+        "rxjs": "^6.5.5",
+        "grapesjs": "0.18.3",
+        "codemirror": "^5.52.0"
+    },
+    "differed": {},
+    "includedInBundle": []
+}
+const externals = {
+    "@youwol/os-core": "window['@youwol/os-core_APIv01']",
+    "@youwol/fv-tree": "window['@youwol/fv-tree_APIv02']",
+    "@youwol/os-top-banner": "window['@youwol/os-top-banner_APIv01']",
+    "@youwol/cdn-client": "window['@youwol/cdn-client_APIv1']",
+    "@youwol/http-clients": "window['@youwol/http-clients_APIv1']",
+    "@youwol/flux-view": "window['@youwol/flux-view_APIv1']",
+    "@youwol/fv-context-menu": "window['@youwol/fv-context-menu_APIv01']",
+    "rxjs": "window['rxjs_APIv6']",
+    "grapesjs": "window['grapesjs_APIv018']",
+    "codemirror": "window['CodeMirror_APIv5']",
+    "rxjs/operators": "window['rxjs_APIv6']['operators']"
+}
+const exportedSymbols = {
+    "@youwol/os-core": {
+        "apiKey": "01",
+        "exportedSymbol": "@youwol/os-core"
+    },
+    "@youwol/fv-tree": {
+        "apiKey": "02",
+        "exportedSymbol": "@youwol/fv-tree"
+    },
+    "@youwol/os-top-banner": {
+        "apiKey": "01",
+        "exportedSymbol": "@youwol/os-top-banner"
+    },
+    "@youwol/cdn-client": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/cdn-client"
+    },
+    "@youwol/http-clients": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/http-clients"
+    },
+    "@youwol/flux-view": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/flux-view"
+    },
+    "@youwol/fv-context-menu": {
+        "apiKey": "01",
+        "exportedSymbol": "@youwol/fv-context-menu"
+    },
+    "rxjs": {
+        "apiKey": "6",
+        "exportedSymbol": "rxjs"
+    },
+    "grapesjs": {
+        "apiKey": "018",
+        "exportedSymbol": "grapesjs"
+    },
+    "codemirror": {
+        "apiKey": "5",
+        "exportedSymbol": "CodeMirror"
+    }
+}
+export const setup = {
+    name:'@youwol/stories',
+        assetId:'QHlvdXdvbC9zdG9yaWVz',
+    version:'0.2.0',
+    shortDescription:"YouWol Stories application",
+    developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/stories',
+    npmPackage:'https://www.npmjs.com/package/@youwol/stories',
+    sourceGithub:'https://github.com/youwol/stories',
+    userGuide:'https://l.youwol.com/doc/@youwol/stories',
+    apiVersion:'02',
+    runTimeDependencies,
+    externals,
+    exportedSymbols
+}
+
+export function getExportedSymbolName(module:string){
+    return `${exportedSymbols[module].exportedSymbol}_APIv${exportedSymbols[module].apiKey}`
+}
