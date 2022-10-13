0f612aeaf21d8c9c4b611abb5dbf446a3a275b6b
youwol-guillaume <greinisch@youwol.com>
2022-10-13 08:03:46 +0200

    :wrench: update template.py & config files

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -1,6 +1,6 @@
 
 const runTimeDependencies = {
-    "load": {
+    "externals": {
         "@youwol/os-core": "^0.1.1",
         "@youwol/fv-tree": "^0.2.3",
         "@youwol/os-top-banner": "^0.1.1",
@@ -9,12 +9,10 @@ const runTimeDependencies = {
         "@youwol/flux-view": "^1.0.3",
         "@youwol/fv-context-menu": "^0.1.1",
         "rxjs": "^6.5.5",
-        "grapesjs": "0.18.3"
-    },
-    "differed": {
+        "grapesjs": "0.18.3",
         "codemirror": "^5.52.0"
     },
-    "includedInBundle": []
+    "includedInBundle": {}
 }
 const externals = {
     "@youwol/os-core": "window['@youwol/os-core_APIv01']",
@@ -71,10 +69,33 @@ const exportedSymbols = {
         "exportedSymbol": "CodeMirror"
     }
 }
+
+// eslint-disable-next-line @typescript-eslint/ban-types -- allow to allow no secondary entries
+const mainEntry : Object = {
+    "entryFile": "./index.ts",
+    "loadDependencies": [
+        "@youwol/os-core",
+        "@youwol/fv-tree",
+        "@youwol/os-top-banner",
+        "@youwol/cdn-client",
+        "@youwol/http-clients",
+        "@youwol/flux-view",
+        "@youwol/fv-context-menu",
+        "rxjs",
+        "grapesjs"
+    ]
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types -- allow to allow no secondary entries
+const secondaryEntries : Object = {}
+const entries = {
+     '@youwol/stories': './index.ts',
+    ...Object.values(secondaryEntries).reduce( (acc,e) => ({...acc, [`@youwol/stories/${e.name}`]:e.entryFile}), {})
+}
 export const setup = {
     name:'@youwol/stories',
         assetId:'QHlvdXdvbC9zdG9yaWVz',
-    version:'0.2.2',
+    version:'0.2.3-wip',
     shortDescription:"YouWol Stories application",
     developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/stories',
     npmPackage:'https://www.npmjs.com/package/@youwol/stories',
@@ -84,7 +105,46 @@ export const setup = {
     runTimeDependencies,
     externals,
     exportedSymbols,
+    entries,
     getDependencySymbolExported: (module:string) => {
         return `${exportedSymbols[module].exportedSymbol}_APIv${exportedSymbols[module].apiKey}`
+    },
+
+    installMainModule: ({cdnClient, installParameters}:{cdnClient, installParameters?}) => {
+        const parameters = installParameters || {}
+        const scripts = parameters.scripts || []
+        const modules = [
+            ...(parameters.modules || []),
+            ...mainEntry['loadDependencies'].map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/stories_APIv02`]
+        })
+    },
+    installAuxiliaryModule: ({name, cdnClient, installParameters}:{name: string, cdnClient, installParameters?}) => {
+        const entry = secondaryEntries[name]
+        const parameters = installParameters || {}
+        const scripts = [
+            ...(parameters.scripts || []),
+            `@youwol/stories#0.2.3-wip~dist/@youwol/stories/${entry.name}.js`
+        ]
+        const modules = [
+            ...(parameters.modules || []),
+            ...entry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        if(!entry){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/stories/${entry.name}_APIv02`]
+        })
     }
 }
--- template.py
+++ template.py
@@ -2,13 +2,25 @@ import shutil
 from pathlib import Path
 
 from youwol.pipelines.pipeline_typescript_weback_npm import Template, PackageType, Dependencies, \
-    RunTimeDeps, generate_template, DevServer
+    RunTimeDeps, generate_template, DevServer, Bundles, MainModule
 from youwol_utils import parse_json
 
 folder_path = Path(__file__).parent
 
 pkg_json = parse_json(folder_path / 'package.json')
 
+load_dependencies = {
+    '@youwol/os-core': '^0.1.1',
+    '@youwol/fv-tree': '^0.2.3',
+    '@youwol/os-top-banner': '^0.1.1',
+    '@youwol/cdn-client': '^1.0.2',
+    '@youwol/http-clients': '^1.0.2',
+    '@youwol/flux-view': '^1.0.3',
+    '@youwol/fv-context-menu': '^0.1.1',
+    'rxjs': '^6.5.5',
+    # do not '^' this version : latter versions have some changes in module definition
+    'grapesjs': '0.18.3',
+}
 
 template = Template(
     path=folder_path,
@@ -19,28 +31,22 @@ template = Template(
     author=pkg_json['author'],
     dependencies=Dependencies(
         runTime=RunTimeDeps(
-            load={
-                '@youwol/os-core': '^0.1.1',
-                '@youwol/fv-tree': '^0.2.3',
-                '@youwol/os-top-banner': '^0.1.1',
-                '@youwol/cdn-client': '^1.0.2',
-                '@youwol/http-clients': '^1.0.2',
-                '@youwol/flux-view': '^1.0.3',
-                '@youwol/fv-context-menu': '^0.1.1',
-                'rxjs': '^6.5.5',
-                # do not '^' this version : latter versions have some changes in module definition
-                'grapesjs': '0.18.3',
-            },
-            differed={
+            externals={
+                **load_dependencies,
+                # differed
                 'codemirror': '^5.52.0',
             }
-        ),
-        devTime={
-        }
+        )
     ),
     userGuide=True,
     devServer=DevServer(
         port=3001
+    ),
+    bundles=Bundles(
+        mainModule=MainModule(
+            entryFile="./index.ts",
+            loadDependencies=list(load_dependencies.keys())
+        )
     )
 )
 
