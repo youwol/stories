b80f2c1bb53e63436ab87ceb95103301045a4366
youwol-guillaume <greinisch@youwol.com>
2022-12-12 17:45:17 +0100

    🎨 app-state => simplify type conversion of LoadingGraph (http-clients.stories -> cdn-client)

--- src/app/app-writer/app-state.ts
+++ src/app/app-writer/app-state.ts
@@ -18,10 +18,7 @@ import {
 import { StoryTopBannerView } from './top-banner'
 import { GrapesEditorView, GrapesEditorState } from './grapes-editor'
 import { map, mapTo, mergeMap } from 'rxjs/operators'
-import {
-    installLoadingGraph,
-    InstallLoadingGraphInputs,
-} from '@youwol/cdn-client'
+import { installLoadingGraph, LoadingGraph } from '@youwol/cdn-client'
 import { Code } from './models'
 import { StructureTab } from '../common/side-nav.view'
 import {
@@ -305,8 +302,9 @@ export class AppState implements AppStateCommonInterface {
                 mergeMap((resp) => {
                     return from(
                         installLoadingGraph({
-                            loadingGraph: resp.requirements.loadingGraph,
-                        } as unknown as InstallLoadingGraphInputs),
+                            loadingGraph: resp.requirements
+                                .loadingGraph as LoadingGraph,
+                        }),
                     )
                 }),
             )
