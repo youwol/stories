ae7f4b777e2b29e1e950e73d7c4842dfb7ecebc8
youwol-guillaume <greinisch@youwol.com>
2022-12-12 12:08:40 +0100

    🔖 0.2.5

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -95,7 +95,7 @@ const entries = {
 export const setup = {
     name:'@youwol/stories',
         assetId:'QHlvdXdvbC9zdG9yaWVz',
-    version:'0.2.5-wip',
+    version:'0.2.5',
     shortDescription:"YouWol Stories application",
     developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/stories',
     npmPackage:'https://www.npmjs.com/package/@youwol/stories',
@@ -130,7 +130,7 @@ export const setup = {
         const parameters = installParameters || {}
         const scripts = [
             ...(parameters.scripts || []),
-            `@youwol/stories#0.2.5-wip~dist/@youwol/stories/${entry.name}.js`
+            `@youwol/stories#0.2.5~dist/@youwol/stories/${entry.name}.js`
         ]
         const modules = [
             ...(parameters.modules || []),
