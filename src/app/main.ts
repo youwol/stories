f7d82545186cc2b270ecbaf0b1e6ce2346a5539a
youwol-guillaume <greinisch@youwol.com>
2022-12-12 12:02:53 +0100

    ♻️ main.ts => dependencies' install use dedicated 'installMainModule'

--- src/app/main.ts
+++ src/app/main.ts
@@ -5,10 +5,12 @@
  * @module main
  */
 
-import { Client, install, LoadingScreenView } from '@youwol/cdn-client'
+import { Client, LoadingScreenView } from '@youwol/cdn-client'
 import { setup } from '../auto-generated'
 require('./style.css')
 export {}
+
+import * as cdnClient from '@youwol/cdn-client'
 
 const searchParams = new URLSearchParams(window.location.search)
 
@@ -16,48 +18,35 @@ const loadingScreen = new LoadingScreenView()
 loadingScreen.render()
 
 if (searchParams.has('mode') && searchParams.get('mode') == 'reader') {
-    const required = ['@youwol/fv-tree', '@youwol/os-top-banner']
-    await install({
-        modules: Object.entries(setup.runTimeDependencies.externals)
-            .filter(([k]) => required.includes(k))
-            .map(([k, v]) => `${k}#${v}`),
-        css: [
-            'bootstrap#4.4.1~bootstrap.min.css',
-            'fontawesome#5.12.1~css/all.min.css',
-            '@youwol/fv-widgets#latest~dist/assets/styles/style.youwol.css',
-        ],
-        onEvent: (ev) => {
-            loadingScreen.next(ev)
+    await setup.installMainModule({
+        cdnClient,
+        installParameters: {
+            css: [
+                'bootstrap#4.4.1~bootstrap.min.css',
+                'fontawesome#5.12.1~css/all.min.css',
+                '@youwol/fv-widgets#latest~dist/assets/styles/style.youwol.css',
+            ],
+            onEvent: (ev) => {
+                loadingScreen.next(ev)
+            },
         },
     })
     Client['initialLoadingScreen'] = loadingScreen
     await import('./load-app-reader')
 } else {
-    const required = [
-        '@youwol/fv-tree',
-        '@youwol/os-top-banner',
-        '@youwol/fv-group',
-        '@youwol/fv-button',
-        '@youwol/fv-tree',
-        '@youwol/fv-tabs',
-        '@youwol/fv-input',
-        '@youwol/fv-context-menu',
-        '@youwol/os-top-banner',
-        'grapesjs',
-    ]
-    await install({
-        modules: Object.entries(setup.runTimeDependencies.externals)
-            .filter(([k]) => required.includes(k))
-            .map(([k, v]) => `${k}#${v}`),
-        css: [
-            'bootstrap#4.4.1~bootstrap.min.css',
-            'fontawesome#5.12.1~css/all.min.css',
-            '@youwol/fv-widgets#latest~dist/assets/styles/style.youwol.css',
-            'highlight.js#11.2.0~styles/default.css',
-            'grapesjs#0.18.3~css/grapes.min.css',
-        ],
-        onEvent: (ev) => {
-            loadingScreen.next(ev)
+    await setup.installMainModule({
+        cdnClient,
+        installParameters: {
+            css: [
+                'bootstrap#4.4.1~bootstrap.min.css',
+                'fontawesome#5.12.1~css/all.min.css',
+                '@youwol/fv-widgets#latest~dist/assets/styles/style.youwol.css',
+                'highlight.js#11.2.0~styles/default.css',
+                'grapesjs#0.18.3~css/grapes.min.css',
+            ],
+            onEvent: (ev) => {
+                loadingScreen.next(ev)
+            },
         },
     })
     Client['initialLoadingScreen'] = loadingScreen
